2d991dd0f657fdf4886281db2e36f9c02d71a185
Alex WIlliam <djcrust@gmail.com>
2017-10-25 12:04:20 +0000

    first commit

---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    
+
+	protected $table = 'posts';
+
+	protected fillable = array ('title', 'content','author_id');
+
+	public $timestamps = true;
+
+
+}
